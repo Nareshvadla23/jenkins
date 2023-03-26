396749828011e1c0d43b24383e5b5b90f9fb5a19
Naresh <naresh28vadla@gmail.com>
2023-03-26 09:57:14 +0530

    test commit

---
+++ src/main/java/com/example/controller/JenkinController.java
@@ -0,0 +1,14 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinController {
+
+	public String testJenkins()
+	{
+		return "Welcome to jenkins World";
+		
+	}
+	
+}
